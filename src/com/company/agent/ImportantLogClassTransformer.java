6836ae7e9ef1a8ce2d6119194c37ac82504adde7
ackhare <bairagi.sirfire@gmail.com>
2016-07-05 08:58:58 +0530

    Second commit code for javassist manipulation
    The companion project TestingAgent is also in git

--- src/com/company/agent/ImportantLogClassTransformer.java
+++ src/com/company/agent/ImportantLogClassTransformer.java
@@ -1,15 +1,109 @@
 package com.company.agent;
 
+import javassist.*;
+import javassist.bytecode.AnnotationsAttribute;
+import javassist.bytecode.MethodInfo;
+import javassist.bytecode.annotation.Annotation;
+import javassist.bytecode.annotation.ArrayMemberValue;
+import javassist.bytecode.annotation.MemberValue;
+import javassist.bytecode.annotation.StringMemberValue;
+
+import java.io.IOException;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.security.ProtectionDomain;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class ImportantLogClassTransformer implements
-ClassFileTransformer {
-	 public byte[] transform(ClassLoader loader, String className,
-	 	 	 Class classBeingRedefined, ProtectionDomain protectionDomain,
-	 	 	 byte[] classfileBuffer) throws IllegalClassFormatException {
-System.out.println(" Loading class: " + className);
-	 	 return null;
-	 }
+        ClassFileTransformer {
+    public byte[] transform(ClassLoader loader, String className,
+                            Class classBeingRedefined, ProtectionDomain protectionDomain,
+                            byte[] classfileBuffer) throws IllegalClassFormatException {
+
+
+        ClassPool pool = ClassPool.getDefault();
+        pool.insertClassPath(new ByteArrayClassPath(className, classfileBuffer));
+        CtClass cclass = null;
+        try {
+            cclass = pool.get(className.replaceAll("/", "."));
+        } catch (NotFoundException e) {
+            e.printStackTrace();
+        }
+        if (!cclass.isFrozen()) {
+            for (CtMethod currentMethod : cclass.getDeclaredMethods()) {
+                Annotation annotation = getAnnotation(currentMethod);
+                if (annotation != null) {
+                    List<String> parameterIndexes = getParamIndexes(annotation);
+                    try {
+                        currentMethod.insertBefore(createJavaString(currentMethod, className, parameterIndexes));
+                    } catch (CannotCompileException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+            try {
+                return cclass.toBytecode();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (CannotCompileException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        return null;
+    }
+
+
+    private Annotation getAnnotation(CtMethod method) {
+        MethodInfo mInfo = method.getMethodInfo();
+// the attribute we are looking for is a runtime invisible attribute
+// use Retention(RetentionPolicy.RUNTIME) on the annotation to make it
+// visible at runtime
+        AnnotationsAttribute attInfo = (AnnotationsAttribute) mInfo
+                .getAttribute(AnnotationsAttribute.invisibleTag);
+        if (attInfo != null) {
+            // this is the type name meaning use dots instead of slashes
+            return attInfo.getAnnotation("com.example.qcon.mains.ImportantLog");
+        }
+        return null;
+    }
+
+    private List<String> getParamIndexes(Annotation annotation) {
+        ArrayMemberValue fields = (ArrayMemberValue) annotation
+                .getMemberValue("fields");
+        if (fields != null) {
+            MemberValue[] values = (MemberValue[]) fields.getValue();
+            List<String> parameterIndexes = new ArrayList<String>();
+            for (MemberValue val : values) {
+                parameterIndexes.add(((StringMemberValue) val).getValue());
+            }
+            return parameterIndexes;
+        }
+        return Collections.emptyList();
+    }
+
+
+    private String createJavaString(CtMethod currentMethod, String className,
+                                    List<String> indexParameters) {
+        StringBuilder sb = new StringBuilder();
+        for (String index : indexParameters) {
+            try {
+                int localVar = Integer.parseInt(index) + 1;
+                sb.append("sb.append(\"\\n Index: \");");
+                sb.append("sb.append(\"");
+                sb.append(index);
+                sb.append("\");sb.append(\" value: \");");
+                sb.append("sb.append($" + localVar + ");");
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+        }
+        sb.append("System.out.println(sb.toString());}");
+        return sb.toString();
+        // $0, $1, $2, ... can be used to access“this” and the actual method parameters
+    }
 }
